f3ce9c195bb44c5857089b102d1cf22b0f711ae4
yingminzhou <yingminzhou@sohu-inc.com>
2018-09-06 23:56:22 +0800

    add two solutions

--- 32-LongestValidParentheses.py
+++ 32-LongestValidParentheses.py
@@ -4,20 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        sLen = len(s)
-        dp = [0 for _ in range(sLen)]
+        parent = list()
         maxLen = 0
-        for i in range(1, sLen):
+        curLen = 0
+        for i in range(len(s)):
             cur = s[i]
-            if cur == ")":
-                pre = s[i-1]
-                if pre == "(":
-                    dp[i] = (dp[i-2] if i >= 2 else 0) + 2
+            if cur == "(":
+                parent.append(cur)
+            else:
+                if len(parent) == 0:
+                    if maxLen < curLen:
+                        maxLen = curLen
+                    curLen = 0
                 else:
-                    preIdx = i - dp[i-1] - 1
-                    if preIdx >= 0:
-                        preMatch = s[preIdx]
-                        if preMatch == "(":
-                            dp[i] = dp[i-1] + 2 + (dp[preIdx-1] if preIdx-1 >= 0 else 0)
-                maxLen = dp[i] if dp[i] > maxLen else maxLen
-        return maxLen
+                    parant = parent[:-1]
+                    curLen += 1
+        if maxLen < curLen:
+            maxLen = curLen
+        return maxLen * 2
+
